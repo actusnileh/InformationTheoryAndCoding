e9140257802bee7bcc7a748ae077d2de06b30d2b
ActusNileh <actusnileh@gmail.com>
2024-04-02 23:02:50 +0300

    Немного интерфейса

--- 1_lab/MainWindow.xaml.cs
+++ 1_lab/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Windows;
+using System.Windows.Media;
 
 namespace deflate_lab
 {
@@ -11,6 +13,9 @@ namespace deflate_lab
     /// </summary>
     public partial class MainWindow : Window
     {
+        string filepath;
+        string filename;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -26,6 +31,28 @@ namespace deflate_lab
             string decompressedString = huffman.Decompress(compressedHuffman);
 
             var result = LZ77.Decompress(decompressedString);
+        }
+
+        void ButtonChooseFile(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog
+            {
+                DefaultExt = ".txt"
+            };
+
+            bool? result = dialog.ShowDialog();
+
+            if (result == true)
+            {
+                filepath = dialog.FileName;
+                SelectedFileTextBox.Text = $"Выбран файл: {dialog.SafeFileName}";
+                SelectedFileTextBox.Foreground = Brushes.Gray;
+            }
+            else
+            {
+                SelectedFileTextBox.Text = "Файл не выбран";
+                SelectedFileTextBox.Foreground = Brushes.LightGray;
+            }
         }
     }
 }
