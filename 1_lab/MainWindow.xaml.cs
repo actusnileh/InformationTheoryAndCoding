daa30e71c70776a616ca92ca69bd0760198ef04d
ActusNileh <actusnileh@gmail.com>
2024-04-05 09:00:09 +0300

    Сделал асинхронным работу кодирования что бы программа не померла и добавил прогрессбар

--- 1_lab/MainWindow.xaml.cs
+++ 1_lab/MainWindow.xaml.cs
@@ -1,14 +1,11 @@
 ﻿using Microsoft.Win32;
-using System;
 using System.IO;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
 
 namespace deflate_lab
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         string filepath;
@@ -41,47 +38,67 @@ namespace deflate_lab
             }
         }
 
-        void Button_Click(object sender, RoutedEventArgs e)
+        async void Button_Click(object sender, RoutedEventArgs e)
         {
             string text, decompressedHuffman, decompressedLZ77;
             byte[] compressedHuffman;
-
-            using (StreamReader file = new StreamReader(filepath))
+            await Task.Run(() =>
             {
-                text = file.ReadToEnd();
-                string LZ77Encode_file = LZ77.Compress(10000, 5000, text);
+                using (StreamReader file = new StreamReader(filepath))
+                {
+                    Dispatcher.Invoke(() => DeflateStartButton.IsEnabled = false);
+                    Dispatcher.Invoke(() => ChooseFileButton.IsEnabled = false);
 
-                Huffman huffman = new Huffman();
-                compressedHuffman = huffman.Compress(LZ77Encode_file);
-                decompressedHuffman = huffman.Decompress(compressedHuffman);
-                decompressedLZ77 = LZ77.Decompress(decompressedHuffman);
+                    text = file.ReadToEnd();
+                    Dispatcher.Invoke(() => ProgressBar.Value = 25);
 
-            }
+                    string LZ77Encode_file = LZ77.Compress(10000, 5000, text);
+                    Dispatcher.Invoke(() => ProgressBar.Value = 50);
 
-            string compressed_filepath = filepath.Replace(".txt", "_compressed.txt");
-            using (FileStream compressed_file = new FileStream(compressed_filepath, FileMode.Create))
-            {
-                compressed_file.Write(compressedHuffman, 0, compressedHuffman.Length);
-            }
-            
+                    Huffman huffman = new Huffman();
+                    compressedHuffman = huffman.Compress(LZ77Encode_file);
+                    Dispatcher.Invoke(() => ProgressBar.Value = 75);
 
-            string decompressed_filepath = filepath.Replace(".txt", "_decompressed.txt");
-            using (StreamWriter decompressed_file = new StreamWriter(decompressed_filepath))
-            {
-                decompressed_file.Write(decompressedLZ77);
-            }
-            SelectedFileTextBox.Text = "Готово";
+                    decompressedHuffman = huffman.Decompress(compressedHuffman);
+                    Dispatcher.Invoke(() => ProgressBar.Value = 85);
 
-            FileInfo SelectedFile = new FileInfo(filepath);
-            long SelectedFileInBytes = SelectedFile.Length;
+                    decompressedLZ77 = LZ77.Decompress(decompressedHuffman);
+                    Dispatcher.Invoke(() => ProgressBar.Value = 95);
+                }
 
-            FileInfo EncodedFile = new FileInfo(compressed_filepath);
-            long EncodedFileInBytes = EncodedFile.Length;
+                string compressed_filepath = filepath.Replace(".txt", "_compressed.txt");
+                using (FileStream compressed_file = new FileStream(compressed_filepath, FileMode.Create))
+                {
+                    compressed_file.Write(compressedHuffman, 0, compressedHuffman.Length);
+                }
 
-            FileInfo DecodedFile = new FileInfo(decompressed_filepath);
-            long DecodedFileInBytes = DecodedFile.Length;
+                string decompressed_filepath = filepath.Replace(".txt", "_decompressed.txt");
+                using (StreamWriter decompressed_file = new StreamWriter(decompressed_filepath))
+                {
+                    decompressed_file.Write(decompressedLZ77);
+                }
 
-            DeflateStartButton.Content = $"{SelectedFileInBytes / 1024} -> {EncodedFileInBytes / 1024} -> {DecodedFileInBytes / 1024}";
+                Dispatcher.Invoke(() =>
+                {
+                    ProgressBar.Value = 100;
+                    SelectedFileTextBox.Text = "Готово";
+
+                    Dispatcher.Invoke(() => DeflateStartButton.IsEnabled = true);
+                    Dispatcher.Invoke(() => ChooseFileButton.IsEnabled = true);
+
+                    FileInfo SelectedFile = new FileInfo(filepath);
+                    long SelectedFileInBytes = SelectedFile.Length;
+
+                    FileInfo EncodedFile = new FileInfo(compressed_filepath);
+                    long EncodedFileInBytes = EncodedFile.Length;
+
+                    FileInfo DecodedFile = new FileInfo(decompressed_filepath);
+                    long DecodedFileInBytes = DecodedFile.Length;
+
+                    DeflateStartButton.Content = $"{SelectedFileInBytes / 1024} -> {EncodedFileInBytes / 1024} -> {DecodedFileInBytes / 1024}";
+                });
+            });
         }
+
     }
 }
